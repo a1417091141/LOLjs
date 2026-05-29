43a940933ae5b9fe70bf40247b0480e415af197c
a1417091141 <44991331+a1417091141@users.noreply.github.com>
2026-05-30 04:01:13 +0800

    Update yindex1.js

--- yindex1.js
+++ yindex1.js
@@ -1,1 +1,23 @@
+// ==UserScript==
+// @name         修改RP显示
+// @namespace    pengu
+// @match        https://riot:*
+// @grant        none
+// @version      1.0.0
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    function modifyRP() {
+        const rpElements = document.querySelectorAll('.currency-rp-top-up-enabled');
+        for (const el of rpElements) {
+            if (el.textContent.trim() !== '999999') {
+                el.textContent = '999999';
+            }
+        }
+    }
+
+    setInterval(modifyRP, 1000);
+    modifyRP();
+})();
